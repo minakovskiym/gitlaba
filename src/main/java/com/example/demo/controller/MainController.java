4e51b6e4145ce75bc839fcc1a136d724c18a628c
minakovskiym <minakovskiy.m@gmail.com>
2023-05-25 19:39:22 +1000

    second commit

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,7 @@ public class MainController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring!";
+        return "Hello, Mikhail Minakovskiy! Today is " +LocalDate.now().toString();
     }
 
 }
